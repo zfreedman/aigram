b4570aeeea052dfd99a19086eddf1ff399274c96
zfreedman <zfreedman92@gmail.com>
2018-05-26 19:10:04 -0700

    feat: Added bootstrap links to index.html for styling and JS

--- src/components/match_bar.js
+++ src/components/match_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class MatchBar extends Component {
   render() {
     return (
-      <div id="match_bar" className="col">
+      <div id="match_bar" className="col-sm">
         Match Bar
       </div>
     );
--- src/components/settings_bar.js
+++ src/components/settings_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SettingsBar extends Component {
   render() {
     return (
-      <div id="settings_bar" className="col">
+      <div id="settings_bar" className="col-sm">
         Settings Bar
       </div>
     );
